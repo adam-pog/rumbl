bead03cc218188a8569feee526fdbd77f10adcfe
Adam Pogwizd <adampog77@gmail.com>
2018-11-02 16:08:00 -0500

    finished up authentication layer with login/out

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -3,6 +3,8 @@ defmodule Rumbl.Accounts do
   the Accounts context
   """
 
+  import Ecto.Query
+
   alias Rumbl.Accounts.User
   alias Rumbl.Repo
 
@@ -20,6 +22,25 @@ defmodule Rumbl.Accounts do
 
   def get_user_by(params) do
     Repo.get_by(User, params)
+  end
+
+  def get_user_by_email(email) do
+    from(u in User, join: c in assoc(u, :credential), where: c.email == ^email)
+    |> Repo.one()
+    |> Repo.preload(:credential)
+  end
+
+  def authenticate_by_email_and_pass(email, given_pass) do
+    user = get_user_by_email(email)
+    cond do
+      user && Comeonin.Pbkdf2.checkpw(given_pass, user.credential.password_hash) ->
+        {:ok, user}
+      user ->
+        {:error, :unauthorized}
+      true ->
+        Comeonin.Bcrypt.dummy_checkpw()
+        {:error, :not_found}
+    end
   end
 
   def change_user(%User{} = user) do
@@ -133,4 +154,6 @@ defmodule Rumbl.Accounts do
   def change_credential(%Credential{} = credential) do
     Credential.changeset(credential, %{})
   end
+
+
 end
