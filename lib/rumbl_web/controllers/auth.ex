52af32d159e3a737a7a9cc191d12cbe49d3470b1
Adam Pogwizd <adampog77@gmail.com>
2018-11-02 15:17:53 -0500

    login user on creation

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -1,7 +1,7 @@
 defmodule RumblWeb.Auth do
   import Plug.Conn
 
-  alias Rmbl.Accounts
+  alias Rumbl.Accounts
 
   def init(opts), do: opts
 
@@ -9,5 +9,12 @@ defmodule RumblWeb.Auth do
     user_id = get_session(conn, :user_id)
     user = user_id && Accounts.get_user(user_id)
     assign(conn, :current_user, user)
+  end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
   end
 end
